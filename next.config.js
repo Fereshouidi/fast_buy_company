cbbc4d36a9ddcb23b43cb153597aaf743b9e8048
fares <fereshouidi298@gmail.com>
2025-02-16 23:45:11 +0100

    edit

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-// // next.config.js
-// module.exports = {
-//     reactStrictMode: true,
-//     swcMinify: true, // استخدام SWC للتقليص
-// };
+// next.config.js
+module.exports = {
+    reactStrictMode: true,
+    swcMinify: true, // استخدام SWC للتقليص
+};
   
-//   // postcss.config.js
-// module.exports = {
-//     plugins: {
-//       autoprefixer: {},
-//       // إذا كنت تستخدم خاصية CSS أخرى مثل Tailwind CSS
-//     },
-//   };
+  // postcss.config.js
+module.exports = {
+    plugins: {
+      autoprefixer: {},
+      // إذا كنت تستخدم خاصية CSS أخرى مثل Tailwind CSS
+    },
+  };
   
