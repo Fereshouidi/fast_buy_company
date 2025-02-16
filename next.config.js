cdab02cb9a9e2d49494d5bbf3ef3eee9b04f8d9f
fares <fereshouidi298@gmail.com>
2025-02-16 23:14:12 +0100

    edit

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 
-  // next.config.js
-module.exports = {
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
-        ],
-      });
+//   // next.config.js
+// module.exports = {
+//     webpack(config) {
+//       config.module.rules.push({
+//         test: /\.css$/,
+//         use: [
+//           'style-loader',
+//           'css-loader',
+//           'postcss-loader',
+//         ],
+//       });
   
-      return config;
-    },
-  };
+//       return config;
+//     },
+//   };
   
